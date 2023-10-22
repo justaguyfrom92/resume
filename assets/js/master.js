29e71c5af84071885fd53596559459f4be5d2777
justaguyfrom92 <justaguyfrom92@gmail.com>
2023-10-22 17:20:14 -0500

    Update master.js

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,9 +1,11 @@
 $( document ).ready(function()
 {
     const myDialog = document.getElementById('d');
-    myDialog.addEventListener('click', (event) => {
-    if (event.target.id !== 'inner-dialog') {
-        myDialog.close();
-    }
-
+    myDialog.addEventListener('click', (event) =>
+    {
+        if (event.target.id !== 'inner-dialog')
+        {
+            myDialog.close();
+        }
+    });
 });

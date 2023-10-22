1e51cccf9a6c6095c3707ca4e3adba6a91c7bbe8
justaguyfrom92 <justaguyfrom92@gmail.com>
2023-10-22 17:21:11 -0500

    Update master.js

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,6 +1,6 @@
 $( document ).ready(function()
 {
-    const myDialog = document.getElementById('d');
+    const myDialog = document.getElementById('inner-dialog');
     myDialog.addEventListener('click', (event) =>
     {
         if (event.target.id !== 'inner-dialog')
